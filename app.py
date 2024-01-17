55f4fa7f60f1e57c9740212a89bdbb9e95f1a082
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-01-18 06:27:04 +0900

    Add register_restaurant function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+restaurants_list = []
 
 def show_app_name():
     print("""
@@ -28,12 +29,20 @@ def invalid_option():
     input("Type ENTER to return to the Main menu.")
     main()
 
+def register_restaurant():
+    os.system("clear")
+    print("Registration of new restaurants\n")
+    restaurant_name = input("Type the desired restaurant name: ")
+    restaurants_list.append(restaurant_name)
+    print(f"The restaurant {restaurant_name} was registered succesfully!\n")
+    input("Type ENTER to return to the Main menu.")
+    main()
 def choose_option():
     try:
         chosen_option = int(input("Choose an option: "))
 
         if chosen_option == 1:
-            print("Register restaurant")
+            register_restaurant()
         elif chosen_option == 2:
             print("List restaurants")
         elif chosen_option == 3:
